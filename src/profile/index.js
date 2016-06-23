d1dc7a7943cf18f071a4129b88684c2b21b96439
Aitor Ibañez <freelance@aitoribanez.com>
2016-06-23 02:08:50 +0200

    start with content

--- src/profile/index.js
+++ src/profile/index.js
@@ -62,6 +62,10 @@ page('/profile', header, function(ctx, next) {
           <a href="#">Media</a>
         </li>
       </ul>
+      <div class="row">
+        <div class="col-sm-3">A</div>
+        <div class="col-sm-9">B</div>
+      </div>
       Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar textos como por ejemplo "Contenido aquí, contenido aquí". Estos textos hacen parecerlo un español que se puede leer. Muchos paquetes de autoedición y editores de páginas web usan el Lorem Ipsum como su texto por defecto, y al hacer una búsqueda de "Lorem Ipsum" va a dar por resultado muchos sitios web que usan este texto si se encuentran en estado de desarrollo. Muchas versiones han evolucionado a través de los años, algunas veces por accidente, otras veces a propósito (por ejemplo insertándole humor y cosas por el estilo).
     </div>
   	<div class="col-sm-3">C</div>
