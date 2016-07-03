6c437922737f37fa9e04bcad9a387a7936b6d119
Aitor Ibañez <freelance@aitoribanez.com>
2016-07-03 04:16:18 +0200

    react-routes successfully addedd

---
+++ pages/PageHome.js
@@ -0,0 +1,11 @@
+/*
+ * Module dependencies
+ */
+
+import React from 'react';
+
+export default class PageHome extends React.Component {	
+	render() { return <p>Esta es la página de Home</p> }
+}
+
+
---
+++ pages/PageProfile.js
@@ -0,0 +1,16 @@
+/*
+ * Module dependencies
+ */
+
+import React from 'react';
+
+import TwityProfileHeaderCard from './../components/TwityProfileHeaderCard/TwityProfileHeaderCard';
+
+export default class PageProfile extends React.Component {	
+	render() { return <div className="col-sm-3">
+     	<TwityProfileHeaderCard />
+   	</div>
+	}
+}
+
+
