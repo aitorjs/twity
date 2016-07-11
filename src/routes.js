2344b33bff858e77692befb61a1a751585d75ff1
Aitor Ibañez <freelance@aitoribanez.com>
2016-07-11 02:25:22 +0200

    routes.js linted too

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import { Router, Route, hashHistory } from 'react-router';
+import React from 'react'
+import { Router, Route, hashHistory } from 'react-router'
 
-import TwityApp from './../components/TwityApp';
+import TwityApp from './../components/TwityApp'
 
-import PageProfile from './../pages/PageProfile';
-import PageHome from './../pages/PageHome';
+import PageProfile from './../pages/PageProfile'
+import PageHome from './../pages/PageHome'
 
 class User extends React.Component {
-  render() {
-    return <p>Esta es la página de User</p>
-  }
+  render () { return <p>Esta es la página de User</p> }
 }
 
 export default
   <Router history={hashHistory}>
     <Route component={TwityApp}>
-      <Route path="/" component={PageHome} />
-      <Route path="user" component={User} />
-      <Route path="profile" component={PageProfile} />
+      <Route path='/' component={PageHome} />
+      <Route path='user' component={User} />
+      <Route path='profile' component={PageProfile} />
     </Route>
-    <Route path="*" component={PageHome} />
+    <Route path='*' component={PageHome} />
   </Router>
