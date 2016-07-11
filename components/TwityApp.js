7fd6a96296e944be1e65a256920a85fef1e39e80
Aitor Ibañez <freelance@aitoribanez.com>
2016-07-11 02:42:23 +0200

    all js linted using ES2015-ES2016

--- components/TwityApp.js
+++ components/TwityApp.js
@@ -2,12 +2,11 @@ import React from 'react'
 // import axios from 'axios'
 
 import TwityNav from './TwityNav/TwityNav'
-import TwityProfileHeaderCard from './TwityProfileHeaderCard/TwityProfileHeaderCard'
 
 import data from './../data.json'
 
-export default class TwityApp extends React.Component {	
-	 constructor(props) {
+export default class TwityApp extends React.Component {
+  constructor (props) {
     super(props)
     this.state = { d: null }
     this.read = this.read.bind(this)
@@ -15,29 +14,28 @@ export default class TwityApp extends React.Component {
 
   async read () {
     try {
-        // const url =  `http://pokeapi.co/api/v1/sprite/1/`
-        // const { data } = await axios.get(url)
-        this.setState({
-          d: data,
-        })
-      } catch (error) {
-        console.error(error)
-      }
+      // const url =  `http://pokeapi.co/api/v1/sprite/1/`
+      // const { data } = await axios.get(url)
+      this.setState({
+        d: data
+      })
+    } catch (error) {
+      console.error(error)
+    }
   }
 
-  componentDidMount() {
+  componentDidMount () {
     this.read()
   }
 
-  render() {     
+  render () {
     if (!this.state.d) {
       return <div>loading</div>
     }
 
     return <div>
-      <br/><br/><br/><br/>
+      <br /><br /><br /><br />
       <TwityNav img={this.state.d.user.img} name={this.state.d.user.name} />
-
       {React.cloneElement(this.props.children, {d: data})}
     </div>
   }
